80a15391bd02080d51086a414ecfbcd1810c3daf
libo <431397516@qq.com>
2021-03-02 00:39:11 +0800

    修改

--- src/view/DesignMode/index.tsx
+++ src/view/DesignMode/index.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { Button } from 'antd';
 import { setLocale } from 'umi';
-import { Subject, Observer } from '@/util/Subject';
-import { Schedule } from '@/util/Schedule';
-import { Observable } from '@reactivex/rxjs';
+// import { Subject, Observer } from '@/util/Subject';
+// import { Schedule } from '@/util/Schedule';
+import { Observable, Subject } from '@reactivex/rxjs';
+import { Observer } from '@/util/Subject';
+// import set = Reflect.set;
 
 type MsgType = string;
 function hello(msg: MsgType) {
@@ -18,7 +20,7 @@ export default () => {
 
   const testA = () => {
     //console.log('我是A');
-    let test = new Subject();
+    /* let test = new Subject();
 
     let person1 = new Observer('person:李大仁');
     let person2 = new Observer('person:王小龙');
@@ -26,12 +28,12 @@ export default () => {
     test.addObserver(person1);
     test.addObserver(person2);
     // test.cancelObserver(person1)
-    test.notify('去杀人');
+    test.notify('去杀人');*/
   };
 
   const testB = () => {
     //console.log('我是A');
-    let appStore = new Schedule();
+    /* let appStore = new Schedule();
     appStore.subscribeProduct('iphone', time => {
       console.log(`手机店关注了新款iphone发布时间为 ${time}`);
     });
@@ -47,7 +49,7 @@ export default () => {
     // appStore.cancelSubscribeProduct('macbookPro');
 
     appStore.publishProduct('iphone', '2021-02-11');
-    appStore.publishProduct('macbookPro', '9999');
+    appStore.publishProduct('macbookPro', '9999');*/
   };
 
   const testC = () => {};
@@ -67,6 +69,71 @@ export default () => {
       });
   }
 
+  const test_rxjs2 = () => {
+    const observable = Observable.create(function(observer) {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      setTimeout(() => {
+        observer.next(4);
+        observer.complete();
+      }, 1000);
+      try {
+        // const b = 123;
+        // b.a = 1;
+      } catch (e) {
+        observer.error(e);
+        //observer.complete();
+      }
+    });
+  };
+
+  const test_rxjs3 = () => {
+    // const observable = Observable.from([10,20,30]);
+    // const subscription = observable.subscribe(x => console.log(x));
+    // subscription.unsubscribe()
+    //     function subscribe(observer) {
+    //       var intervalID = setInterval(() => {
+    //         observer.next('hi');
+    //       }, 100);
+    //
+    //       return function unsubscribe() {
+    //         clearInterval(intervalID);
+    //       };
+    //     }
+    //
+    //     var unsubscribe = subscribe({next: (x) => console.log(x)});
+    //
+    // // 稍后：
+    //     setTimeout(() => {
+    //       unsubscribe(); // 清理资源
+    //     },2000)
+    const observable = Observable.interval(1000);
+    const subscription = observable.subscribe(x => console.log(x));
+    setTimeout(() => {
+      subscription.unsubscribe();
+    }, 3000);
+  };
+
+  const test_subject1 = () => {
+    const subject = new Subject();
+    subject.subscribe({
+      next: x => console.log(`observerA: ` + x),
+    });
+    subject.subscribe({
+      next: v => console.log('observerB: ' + v),
+    });
+
+    // subject.next(1)
+    // subject.next(2)
+    // const observable = Observable.from([1,2,3])
+    const observable = Observable.interval(1000);
+    observable.subscribe(subject);
+    setTimeout(() => {
+      subject.complete();
+    }, 4000);
+  };
+
   return (
     <div className={``}>
       <h1>我是测试设计模式</h1>
@@ -74,6 +141,9 @@ export default () => {
       <Button onClick={testB}>发布订阅模式</Button>
       <Button onClick={testC}>测试RXJS</Button>
       <div id="button1">点击触发rxjs事件</div>
+      <Button onClick={test_rxjs2}>点击触发rxjs2</Button>
+      <Button onClick={test_rxjs3}>点击触发rxjs3</Button>
+      <Button onClick={test_subject1}>点击触发subject1</Button>
     </div>
   );
 };
