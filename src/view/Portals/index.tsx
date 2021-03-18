ddaccf9bf308ac35d7c46c2cf51db00d95e2ddf8
bo.li1 <bo.li1@mobilemd.cn>
2021-03-18 13:06:29 +0800

    添加拖拽功能

--- src/view/Portals/index.tsx
+++ src/view/Portals/index.tsx
@@ -1,29 +1,57 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.less';
+import Draggable from 'react-draggable';
 
-const Modal = ({ message, isOpen, onClose, children }) => {
+const Modal = ({ message, isOpen, children, ...props }) => {
+  const onBGClose = e => {
+    console.log('背景点击被触发了');
+    props.onClose();
+  };
+  const goDrag = e => {
+    e.stopPropagation();
+    // e.can
+    e.preventDefault();
+    e.cancelBubble = true;
+  };
+  const onClose = e => {
+    e.stopPropagation();
+    // e.can
+    e.preventDefault();
+    e.cancelBubble = true;
+    props.onClose();
+  };
   if (!isOpen) return null;
   return ReactDOM.createPortal(
-    <div className="portal-modal" onClick={onClose}>
-      <div className={'modal-win'}>
-        <div className={'portal-header'}>
-          <span className={'title'}>{message}</span>
-          <button className={'close-btn'} onClick={onClose}>
-            Close
-          </button>
-        </div>
-        <div className={'content-box'}>{children}</div>
-        <div className={'footer'}>
-          <button className={'close-btn'} onClick={onClose}>
-            取消
-          </button>
-          <button className={'close-btn'} onClick={onClose}>
-            确定
-          </button>
-        </div>
+    <React.Fragment>
+      <div className="portal-modal" onClick={onBGClose}>
+        <Draggable
+          // defaultPosition={{x: 25, y: 25}}
+          onMouseDown={e => {
+            // e.preventDefault()
+            // e.cancelBubble = true
+          }}
+        >
+          <div className={'modal-win'} onClick={goDrag}>
+            <div className={'portal-header'}>
+              <span className={'title'}>{message}</span>
+              <button className={'close-btn'} onClick={onClose}>
+                Close
+              </button>
+            </div>
+            <div className={'content-box'}>{children}</div>
+            <div className={'footer'}>
+              <button className={'close-btn'} onClick={onClose}>
+                取消
+              </button>
+              <button className={'close-btn'} onClick={onClose}>
+                确定
+              </button>
+            </div>
+          </div>
+        </Draggable>
       </div>
-    </div>,
+    </React.Fragment>,
     document.body,
   );
 };
