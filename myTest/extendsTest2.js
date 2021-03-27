ad8cfb3a4b8f4e74349647684951ad9237ac8daa
libo <431397516@qq.com>
2021-03-27 19:47:36 +0800

    下盖

--- myTest/extendsTest2.js
+++ myTest/extendsTest2.js
@@ -10,3 +10,24 @@ sub1.list.push('d');
 console.log('sub1', sub1.list);
 const sub2 = new Sub();
 console.log('sub2', sub2.list);
+
+const arr = [1, 2, 3, 4, 5];
+
+function getAll(arr) {
+  let add = 0;
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  while (arr.length > 1) {
+    const sum = arr[0] + arr[1];
+    add += sum;
+    arr.splice(0, 2);
+    arr.push(sum);
+    if (arr.length === 1) {
+      break;
+    }
+  }
+  return add;
+}
+
+console.log(getAll(arr));
